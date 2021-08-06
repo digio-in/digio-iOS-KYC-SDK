dc3e35e5ecf7bb69c7ba7da10449516c16a8439c
Akash <akash-at-116522020260>
2021-08-06 16:18:01 +0530

    Digio ios kyc sdk

---
+++ DigiokycSDK.framework/Headers/DigiokycSDK.h
@@ -0,0 +1,18 @@
+//
+//  DigiokycSDK.h
+//  DigiokycSDK
+//
+//  Created by Akash on 26/07/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DigiokycSDK.
+FOUNDATION_EXPORT double DigiokycSDKVersionNumber;
+
+//! Project version string for DigiokycSDK.
+FOUNDATION_EXPORT const unsigned char DigiokycSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DigiokycSDK/PublicHeader.h>
+
+
